1d99605e67e3f4e908dc25ccaf869f640ca83236
amezaki1007 <mkwamezaki1007@gmail.com>
2021-05-13 12:02:23 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import * as ReactDom from 'react-dom';
 //import * as ReactPDF from 'react-pdf';
 import {Document, Page, pdfjs} from 'react-pdf/dist/umd/entry.webpack';
 
-pdfjs.GlobalWorkerOptions.workerSrc = "pdf.worker.js";
+//pdfjs.GlobalWorkerOptions.workerSrc = "pdf.worker.js";
 
 ReactDom.render(
-  <Document file="./xxx.pdf" options={{cMapUrl: "cmaps/", cMapPacked: true,}}>
+  <Document file="../resources/xxx.pdf" options={{cMapUrl: "cmaps/", cMapPacked: true,}}>
     <Page pageNumber={1} />
   </Document>,
   document.getElementById('root')
