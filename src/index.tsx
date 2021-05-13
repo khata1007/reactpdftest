7c26c5db5d6d17e040f820c2a716c142cb340691
amezaki1007 <mkwamezaki1007@gmail.com>
2021-05-13 10:33:12 +0900

    日本語のないpdfをローカルから表示

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,13 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import * as ReactPDF from 'react-pdf';
+//import * as ReactPDF from 'react-pdf';
+import {Document, Page, pdfjs} from 'react-pdf/dist/umd/entry.webpack';
 
-//import './@styles/react-pdf.styl'
+//pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
-// Hello コンポーネントの属性（プロパティ）を定義
-interface HelloProps {
-  name?: string;  // オプショナルな name 属性
-}
-
-// Hello コンポーネントを定義
-class Hello extends React.Component<HelloProps> {
-  public render(): React.ReactNode {
-    const name = this.props.name ?? 'Unknown';
-    return (
-      <b>Hell, {name}!</b>
-    );
-  }
-}
-
-// Hello コンポーネントを <div id="root"> に表示
 ReactDom.render(
-  <ReactPDF.Document file="../resources/xxx.pdf">
-    <ReactPDF.Page pageNumber={1} />
-  </ReactPDF.Document>,
+  <Document file="./xxx.pdf" >
+    <Page pageNumber={1} />
+  </Document>,
   document.getElementById('root')
 );
